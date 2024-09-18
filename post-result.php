e8a252175797233df99971da4e65dfef8233970d
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-09-18 13:46:15 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -2,16 +2,20 @@
 $pageTitle = "Post result";
 include "view-header.php";
 ?>
-    <h1>Post result</h1>
+<h1>Post result</h1>
 <?php
-echo getDisplay();
-include "view-footer.php";
-
-function getDisplay() {
-    if (isset($_POST['my-name'])) {
-      return"<p>The value sent is:</p>" . $_POST['my-name'];
-} else {
-    return"<p>Nothing posted to the page.</p>";
-}
-}
+if (isset($_POST['my-name'])) {
 ?>
+    <p>The value send is:</p>
+<?php
+    echo $_POST['my-name'];
+} else {
+?>
+    <p>Nothing posted to the page.</p>
+<?php
+}
+include "view-footer.php";
+?>
+
+
+
