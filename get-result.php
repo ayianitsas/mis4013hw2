992e3b41a5dc6a5bc8afaa9cbd964095324b533c
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-09-23 23:25:15 -0500

    Update get-result.php

--- get-result.php
+++ get-result.php
@@ -12,7 +12,7 @@ if (isset($_GET['my-name'])) {
 <?php
 } else {
 ?>
-  <p style="color: white; font-weight: bold;">Nothing sent to the page.</p>
+  <p>Nothing sent to the page.</p>
 <?php 
 }
 include "view-footer.php";
