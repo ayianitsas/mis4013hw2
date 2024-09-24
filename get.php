eca952208d5188005d80386301c70a1c73e57d96
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-09-23 22:34:43 -0500

    Update get.php

--- get.php
+++ get.php
@@ -2,11 +2,13 @@
 $pageTitle = "Get";
 include "view-header.php";
 ?>
+<body style="background-color: lightpurple;">
     <h1>Get</h1>
-<form method="get" action="get.result.php">
-  <input type="text" name="my-name">
-  <input type="submit" value="Get data">
-</form>
+    <form method="get" action="get.result.php">
+          <input type="text" name="my-name">
+          <input type="submit" value="Get data">
+    </form>
 <?php
 include "view-footer.php";
 ?>
+</body>
