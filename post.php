41a85eba188e3ea0f3e24f6e19b7c13541370127
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-09-23 22:29:16 -0500

    Update post.php

--- post.php
+++ post.php
@@ -2,6 +2,7 @@
 $pageTitle = "Post";
 include "view-header.php";
 ?>
+<body style="background-color: lightgreen;">
 <h1>Post</h1>
 <form method="post" action="post-result.php">
 
@@ -14,3 +15,4 @@ include "view-header.php";
 <?php
 include "view-footer.php";
 ?>
+</body>
