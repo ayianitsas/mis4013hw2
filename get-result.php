297a20f956edbdf3e49b535e83be6ee337271913
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-09-23 22:38:21 -0500

    Update get-result.php

--- get-result.php
+++ get-result.php
@@ -2,17 +2,19 @@
 $pageTitle = "Get result";
 include "view-header.php";
 ?>
+<body style="background-color: lightpurple;">
     <h1>Get result</h1>
 <?php
 if (isset($_GET['my-name'])) {
 ?>
   <p>The value sent is:</p>
-  <?php
-  echo $_GET['my-name'];
+    <p><?php echo htmlspecialchars($_GET['my-name']); ?></p>
+<?php
 } else {
 ?>
   <p>Nothing sent to the page.</p>
-  <?php 
+<?php 
 }
 include "view-footer.php";
 ?>
+</body>
