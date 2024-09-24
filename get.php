408f950e4dc749aee3f260bb244aa9f57aeda3f8
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-09-23 23:26:37 -0500

    Update get.php

--- get.php
+++ get.php
@@ -2,7 +2,7 @@
 $pageTitle = "Get";
 include "view-header.php";
 ?>
-<body style="background-color: indigo;">
+<body style="background-color: green;">
     <h1 style="color: white; font-weight: bold;">Get</h1>
     <form method="get" action="get.result.php">
           <input type="text" name="my-name">
